cfa629a49e8d5866ffc9bb926c87c0d4299de8ba
elvinavillanoza <elvinavillanoza@gmail.com>
2021-02-17 01:23:08 +0800

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,43 +5,6 @@ import { trigger, state, style, transition, animate, keyframes, group } from '@a
   selector: 'app-root',
   templateUrl: './app.component.html',
   animations: [
-    trigger('divState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0)'
-      })),
-      state('highlighted', style({
-        backgroundColor: 'blue',
-        transform: 'translateX(100px)'
-      })),
-      transition('normal <=> highlighted', animate(300)),
-      // transition('highlighted => normal', animate(800))
-    ]),
-    trigger('wildState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0) scale(1)'
-      })),
-      state('highlighted', style({
-        backgroundColor: 'blue',
-        transform: 'translateX(100px) scale(1)'
-      })),
-      state('shrunken', style({
-        backgroundColor: 'green',
-        transform: 'translateX(0px) scale(0.5)'
-      })),
-      transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800)),
-      transition('shrunken <=> *', [
-        style({
-          'background-color': 'orange'
-        }),
-        animate(1000, style({
-          borderRadius: '50px'
-        })),
-        animate(500)
-      ])
-    ]),
     trigger('list1', [
       state('in', style({
         opacity: 1,
@@ -60,54 +23,13 @@ import { trigger, state, style, transition, animate, keyframes, group } from '@a
           opacity: 0
         }))
       ])
-    ]),
-    trigger('list2', [
-      state('in', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('void => *', [
-        animate(1000, keyframes([
-          style({
-            transform: 'translateX(-100px)',
-            opacity: 0,
-            offset: 0
-          }),
-          style({
-            transform: 'translateX(-50px)',
-            opacity: 0.5,
-            offset: 0.3
-          }),
-          style({
-            transform: 'translateX(-20px)',
-            opacity: 1,
-            offset: 0.8
-          }),
-          style({
-            transform: 'translateX(0px)',
-            opacity: 1,
-            offset: 1
-          })
-        ]))
-      ]),
-      transition('* => void', [
-        group([
-          animate(300, style({
-            color: 'red'
-          })),
-          animate(800, style({
-            transform: 'translateX(100px)',
-            opacity: 0
-          }))
-        ])
-      ])
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';
   wildState = 'normal';
-  list = ['Milk', 'Sugar', 'Bread'];
+  list = [];
 
   onAnimate() {
     this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
